a276d0e5d1407ea39a07f1ccc6ffa4a39f305763
Kainat Amjad Janjua <janjuakainat59@gmail.com>
2024-12-18 22:21:44 +0500

    Add files via upload

---
+++ Mytheme/footer.php
@@ -0,0 +1,43 @@
+<footer id="footer">
+    <div class="footer-container">
+        <div class="footer-section">
+            <h3>Shop SVESTON</h3>
+            <ul>
+                <li><a href="#">Shop All</a></li>
+                <li><a href="#">12 12 Sale</a></li>
+                <li><a href="#">Men Watches</a></li>
+                <li><a href="#">Ladies Watches</a></li>
+                <li><a href="#">Couple Watches</a></li>
+            </ul>
+        </div>
+        <div class="footer-section">
+            <h3>Customer Care</h3>
+            <ul>
+                <li><a href="#">FAQS</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Returns & Exchange Policy</a></li>
+                <li><a href="#">Retail Policy</a></li>
+                <li><a href="#">Warranty Information</a></li>
+                <li><a href="#">Shipping Policy</a></li>
+                <li><a href="#">Modes of Payment</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Service</a></li>
+            </ul>
+        </div>
+        <div class="footer-section">
+            <h3>About Us</h3>
+            <ul>
+                <li><a href="#">Our Story</a></li>
+                <li><a href="#">Store Locator</a></li>
+                <li><a href="#">U.A.E Office</a></li>
+                <li><a href="#">Global Outlets</a></li>
+            </ul>
+        </div>
+    </div>
+        <p>&copy; <?php echo date(format:'Y');?>
+    My wordpress theme.All rights reserved.</p>
+    <p>Developed by <a href="https://example.com">Kainat Amjad</a></p>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
